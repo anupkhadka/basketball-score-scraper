6d34c7ed69949add521c294fb15d59eeef501b27
anupkhadka <akhadka.dev@gmail.com>
2019-05-17 21:47:44 -0400

    Create new gem using bundler

---
+++ lib/basketball_score_scraper.rb
@@ -0,0 +1,6 @@
+require "basketball_score_scraper/version"
+
+module BasketballScoreScraper
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/basketball_score_scraper/version.rb
@@ -0,0 +1,3 @@
+module BasketballScoreScraper
+  VERSION = "0.1.0"
+end
