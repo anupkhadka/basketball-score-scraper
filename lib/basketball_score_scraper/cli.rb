496aa2ea6fb8471563b23cc17f37ba3bb57b1a8d
anupkhadka <akhadka.dev@gmail.com>
2019-05-18 14:44:52 -0400

    Implement run method inside CLI

--- lib/basketball_score_scraper/cli.rb
+++ lib/basketball_score_scraper/cli.rb
@@ -1,6 +1,13 @@
 class BasketballScoreScraper::CLI
 
   def self.run
+    games_data = BasketballScoreScraper::Scraper.scrap
+    BasketballScoreScraper::Game.create_from_collection(games_data)
+    if BasketballScoreScraper::Game.all.count == 0
+      puts "No games were played last night."
+    else
+      BasketballScoreScraper::Game.all.each {|game| game.display}
+    end
   end
 
 end
